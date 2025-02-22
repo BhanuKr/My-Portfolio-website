9c557931a78a2eda948674323c663e75faf811fb
BhanuKr <115964594+BhanuKr@users.noreply.github.com>
2025-02-22 14:08:02 +0530

    Landing Page of Website Completed

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function toggleMenu() {
 }
 
 var typed = new Typed("#animated-text", {
-  strings: ["a Mathematics and Computing Student at IISc", "Passionate About Intelligent Systems", "an Engineer at Heart", "a Science and Tech Enthusiast"],
-  typeSpeed: 30,      // Speed of typing
-  backSpeed: 50,      // Speed of deleting
+  strings: ["a Mathematics and Computing undergrad at IISc", "passionate about Intelligent Systems", "an Engineer at Heart", "a Science and Tech Enthusiast"],
+  typeSpeed: 40,      // Speed of typing
+  backSpeed: 60,      // Speed of deleting
   backDelay: 3000,    // Delay before deleting
   loop: true          // Repeat the animation
 })
